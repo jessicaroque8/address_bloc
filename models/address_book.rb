58f49516e3f1a41c35d32a7f94b7008ca7f7dfd4
Jessica Roque <jroque.dev@gmail.com>
2017-11-29 13:50:23 -0800

    Assignment 4 Revision: Deleting entry inside the each loop.

--- models/address_book.rb
+++ models/address_book.rb
@@ -24,15 +24,16 @@ class AddressBook
    end
 
    def remove_entry(name, phone_number, email)
-      delete_entry = nil
+      # delete_entry = nil
 
       entries.each do |entry|
          if name == entry.name && phone_number == entry.phone_number && email == entry.email
-            delete_entry = entry
+            entries.delete(entry)
+            # delete_entry = entry
          end
       end
 
-      entries.delete(delete_entry)
+      # entries.delete(delete_entry)
    end
 
 end
